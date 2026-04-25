58829a8d0d9daae2eef72ad347fbcbb397abfd65
Ansh Bhatia <anshbhatia@Anshs-MacBook-Air-4.local>
2026-04-25 13:42:00 +0530

    Fix Tina admin bundle drift and schema artifact sync

--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -3338,7 +3338,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "http://localhost:4001/graphql",
+        url: "https://content.tinajs.io/2.2/content/49f9be90-ac1f-435f-9e52-6730dfaacb21/github/main",
         queries,
       })
     )
